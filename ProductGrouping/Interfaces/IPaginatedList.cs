d3b200a5c81138864fc4f1925f8d20b1b24b2954
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2018-11-26 15:59:45 +0000

    added paginatedList files

---
+++ ProductGrouping/Interfaces/IPaginatedList.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductGrouping.Interfaces
+{
+    public interface IPaginatedList
+    {
+        bool HasNextPage { get; }
+
+        bool HasPreviousPage { get; }
+
+        int PageIndex { get; }
+
+        int TotalPages { get; }
+    }
+}
