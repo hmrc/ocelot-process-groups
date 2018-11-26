724d026f7b5cbb1a7455e7813059b6c75e149231
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2018-11-26 12:43:15 +0000

    inital product grouping model created

---
+++ ProductGrouping/Models/ProductGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductGrouping.Models
+{
+    public class ProductGroup
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("Product Reference")]
+        [Required(ErrorMessage = "Product Reference Required")]
+        [MaxLength(10, ErrorMessage = "Product Reference Too Long")]
+        public string ProductReference { get; set; }
+
+        [DisplayName("Product Owner")]
+        [Required(ErrorMessage = "Product Owner Required")]
+        [MaxLength(7, ErrorMessage = "Product Owner Too Long")]
+        public string ProductOwner { get; set; }
+
+        [DisplayName("Group")]
+        [Required(ErrorMessage = "Group Required")]
+        [MaxLength(100, ErrorMessage = "Product Owner Too Long")]
+        public string Group { get; set; }
+
+        [DisplayName("Site")]
+        [Required(ErrorMessage = "Site Required")]
+        [MaxLength(10, ErrorMessage = "Site Too Long")]
+        public string Site { get; set; }
+    }
+}
