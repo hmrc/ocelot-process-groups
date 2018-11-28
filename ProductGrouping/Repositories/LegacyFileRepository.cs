60117d829c5484e68cdf3eaa51502b76ffd80ff7
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2018-11-28 14:02:06 +0000

    remove unneeded usings

--- ProductGrouping/Repositories/LegacyFileRepository.cs
+++ ProductGrouping/Repositories/LegacyFileRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using ProductGrouping.Interfaces;
-using ProductGrouping.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
